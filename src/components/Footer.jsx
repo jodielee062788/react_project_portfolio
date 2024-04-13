347dc0d386d68f0da138d1f2843c6d4852a4b051
Jodie <jodielee062788@gmail.com>
2024-04-14 07:41:32 +1000

    Modify jsx and css files for the view

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+
+
+function Footer() {
+  return (
+    <footer>
+      <p>Developer GitHub</p>
+      <p>Developer LinkedIn</p>
+      <p>Developer Third Platform</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import Navigation from './Navigation';
+
+function Header() {
+  return (
+    <header>
+      <h1>JCLee</h1>
+      <nav>
+        <Navigation />
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
