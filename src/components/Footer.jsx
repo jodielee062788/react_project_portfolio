e6914d61cd3ebc8f04149189412573f6a1baf4e4
Jodie <jodielee062788@gmail.com>
2024-04-15 02:20:10 +1000

    Edit footer code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,23 +3,23 @@
 export default function Footer() {
   return (
     <footer className="p-5">
-      <div className="logos">
-      <a href="mailto:jodielee062788@gmail.com" title="E-mail">
-        <img src="./img/envelope.png" alt="email logo" />
-      </a>
-      <a href="https://github.com/jodielee062788" title="Github">
-        <img src="./img/github.png" alt="github logo" />
-      </a>
-      <a href="https://www.twitter.com/jodieleeleelee" title="Twitter">
-        <img src="./img/twitter.png" alt="twitter logo" />
-      </a>
-      <a href="https://www.instagram.com/jodieleely" title="Instagram">
-        <img src="./img/instagram.png" alt="instagram logo" />
-      </a>
-      <a href="https://www.linkedin.com/in/jodie-lee-a34245263/" title="LinkedIn">
-        <img src="./img/linkedin.png" alt="linkedin logo" />
-      </a>
-    </div>
+      <div className="logos d-flex justify-content-center">
+        <a href="mailto:jodielee062788@gmail.com" title="E-mail">
+          <img src="./img/envelope.png" alt="email logo" />
+        </a>
+        <a href="https://github.com/jodielee062788" title="Github">
+          <img src="./img/github.png" alt="github logo" />
+        </a>
+        <a href="https://www.twitter.com/jodieleeleelee" title="Twitter">
+          <img src="./img/twitter.png" alt="twitter logo" />
+        </a>
+        <a href="https://www.instagram.com/jodieleely" title="Instagram">
+          <img src="./img/instagram.png" alt="instagram logo" />
+        </a>
+        <a href="https://www.linkedin.com/in/jodie-lee-a34245263/" title="LinkedIn">
+          <img src="./img/linkedin.png" alt="linkedin logo" />
+        </a>
+      </div>
     </footer>
   );
 }
