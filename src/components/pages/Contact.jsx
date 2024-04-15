4a0fa7b36aeb8acb973f08ae57075d51d1a008a6
Jodie <jodielee062788@gmail.com>
2024-04-15 23:03:53 +1000

    Edit design of contact and about page

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -22,47 +22,49 @@ export default function Contact() {
 
   return (
     <div className='contact d-flex flex-column align-items-center mx-auto'>
-      <div className="contact-logo">
-        <img src="./img/cat3.png" alt="cat logo" />
-        <h1 className='ms-3'>Contact</h1>
-      </div>
-      <form onSubmit={handleSubmit}>
-        <div className='mt-4 d-flex flex-column align-items-start'>
-          <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-            required
-          />
+      <div className="contact1 d-inline-block border border-dark rounded p-3 shadow">
+        <div className="contact-logo align-items-center justify-content-center">
+          <img src="./img/cat3.png" alt="cat logo" />
+          <h1 className='ms-3 mt-2'>Contact</h1>
         </div>
-        <div className='d-flex flex-column align-items-start mt-3'>
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className='d-flex flex-column align-items-start mt-3'>
-          <label htmlFor="message">Message</label>
-          <textarea
-            id="message"
-            name="message"
-            value={formData.message}
-            onChange={handleChange}
-            required
-          ></textarea>
-        </div>
-        <div>
-          <button className='d-flex mx-auto mt-4 fw-bold' type="submit">SUBMIT</button>
-        </div>
-      </form>
+        <form onSubmit={handleSubmit}>
+          <div className='mt-4 d-flex flex-column align-items-start'>
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className='d-flex flex-column align-items-start mt-3'>
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className='d-flex flex-column align-items-start mt-3'>
+            <label htmlFor="message">Message</label>
+            <textarea
+              id="message"
+              name="message"
+              value={formData.message}
+              onChange={handleChange}
+              required
+            ></textarea>
+          </div>
+          <div>
+            <button className='d-flex mx-auto mt-4 fw-bold' type="submit">SUBMIT</button>
+          </div>
+        </form>
+      </div>  
     </div>
   );
 }
