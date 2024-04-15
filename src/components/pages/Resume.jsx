4b01e6b995d5765852a11510bfcc59d01a0fc814
Jodie <jodielee062788@gmail.com>
2024-04-16 09:08:27 +1000

    Add proficiency logos, edit resume page

--- src/components/pages/Resume.jsx
+++ src/components/pages/Resume.jsx
@@ -1,35 +1,87 @@
 import Footer from '../Footer';
 
 export default function Resume() {
-  const proficiencies = [
-    'JavaScript',
-    'React',
-    'Node.js',
-    'HTML',
-    'CSS',
-    'Git',
-    'SQL',
-    'RESTful APIs',
+  const frontendProficiencies = [
+    { name: 'React', logo: './img/react.png' },
+    { name: 'HTML', logo: './img/html.png' },
+    { name: 'CSS', logo: './img/css.png' },
+    { name: 'JavaScript', logo: './img/javascript.png' },
+    { name: 'jQuery', logo: './img/jquery1.png' },
+    { name: 'Vue.js', logo: './img/vue.png' },
+    { name: 'Vite', logo: './img/vite.png' },
+    { name: 'Bootstrap', logo: './img/bootstrap.png' },
+    { name: 'Tailwind CSS', logo: './img/tailwind1.png' },
+  ];
+
+  const backendProficiencies = [
+    { name: 'Node.js', logo: './img/node-js.png' },
+    { name: 'Express.js', logo: './img/express.png' },
+    { name: 'MySQL', logo: './img/database.png' },
+    { name: 'Sequelize', logo: './img/sequelize.png' },
+    { name: 'MongoDB', logo: './img/mongodb1.png' },
+    { name: 'GraphQL', logo: './img/graphql.png' },
+    { name: 'Python', logo: './img/python.png' },
+    { name: 'Java', logo: './img/web.png' },
+  ];
+
+  const otherTechnologies = [
+    { name: 'Git', logo: './img/git.png' },
+    { name: 'GitHub', logo: './img/github.png' },
+    { name: 'GitLab', logo: './img/gitlab.png' },
+    { name: 'Webpack', logo: './img/webpack.png' },
+    { name: 'Babel', logo: './img/babel.png' },
+    { name: 'Jest', logo: './img/jest.png' },
+    { name: 'Heroku', logo: './img/heroku.png' },
+    { name: 'Netlify', logo: './img/netlify.png' },
   ];
 
   return (
-    <div className='resume d-flex flex-column align-items-center mx-auto'>
+    <div className='resume d-flex flex-column align-items-center'>
       <div className="resume-logo">
-        <img src="./img/cat4.png" alt="" />
-        <h1>Resume</h1>
+        <img src="./img/cat4.png" alt="cat logo" />
+        <h1 className='ms-3 fw-bold'>Resume</h1>
       </div>
       <p>
         Download my resume:{' '}
-        <a href="/path/to/resume.pdf" download>
+        <a href="/img/resume.pdf" download>
           Resume.pdf
         </a>
       </p>
-      <h3>Proficiencies</h3>
-      <ul>
-        {proficiencies.map((skill, index) => (
-          <li key={index}>{skill}</li>
-        ))}
-      </ul>
+      <div className="proficiencies row">
+        <div className="col-md-4 proficiency-section">
+          <h3>Frontend Proficiencies</h3>
+          <ul className="proficiency-list">
+            {frontendProficiencies.map((skill, index) => (
+              <li key={index}>
+                <img src={skill.logo} alt={`${skill.name} logo`} className="proficiency-logo mb-2" />
+                {skill.name}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="col-md-4 proficiency-section">
+          <h3>Backend Proficiencies</h3>
+          <ul className="proficiency-list">
+            {backendProficiencies.map((skill, index) => (
+              <li key={index}>
+                <img src={skill.logo} alt={`${skill.name} logo`} className="proficiency-logo mb-2" />
+                {skill.name}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="col-md-4 proficiency-section">
+          <h3>Development<br></br>Tools</h3>
+          <ul className="proficiency-list">
+            {otherTechnologies.map((tech, index) => (
+              <li key={index}>
+                <img src={tech.logo} alt={`${tech.name} logo`} className="proficiency-logo mb-2" />
+                {tech.name}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
       <Footer />
     </div>
   );
